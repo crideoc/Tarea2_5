a23e779b41993810b84f09035c5cd0b89278d4e3
crideoc <71828356+crideoc@users.noreply.github.com>
2022-11-15 11:13:58 +0100

    no funciona , error llamarIntent

--- app/src/main/java/com/example/tarea2_5/MainActivity.java
+++ app/src/main/java/com/example/tarea2_5/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -15,10 +19,14 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView tv1;
     private ImageView im1;
+    private static final int REQUEST_CALL_PHONE = 0;
+    private static final int REQUEST_IMAGE_CAPTURE = 1 ;
+    private Intent intent=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tv1=findViewById(R.id.txtMostrar);
         im1=findViewById(R.id.imageView);
+
 
     }
 
@@ -36,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void llamarIntent(View vista){
-        Intent intent=null;
 
         switch(vista.getId()){
             case R.id.btnLanzarNavegador:
@@ -44,8 +52,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 break;
             case R.id.btnLanzarContacto:
-                intent=new Intent(Intent.ACTION_CALL, Uri.parse("tel:(+34)555900800"));
-                startActivity(intent);
+                if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.CALL_PHONE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // No tenemos permiso.¿Debemos mostrar una explicación para pedir el permiso?
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.CALL_PHONE)) {
+                        // Muestra una explicación del porqué pedir el permiso.
+                        Toast.makeText(this, "Se necesita permiso para realizar llamadas",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    // Solicitamos el permiso.
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.CALL_PHONE},
+                            REQUEST_CALL_PHONE);
+                } else {
+                    //Tenemos permiso.
+                    llamar();
+                }
                 break;
             case R.id.btnMarcarTelefono:
                 intent=new Intent(Intent.ACTION_DIAL, Uri.parse("tel:(+34)555900800"));
@@ -62,9 +86,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 break;
             case R.id.btnTomarFoto:
-                intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                // Lanzamos la actividad y esperamos su resultado
-                lanzadorActividades.launch(intent);
+                if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.CAMERA)) {
+                        // Muestra una explicación del por qué de perdir el permiso.
+                        Toast.makeText(this, "Se necesita permiso para acceder a la cámara.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    // Solicitamos el permiso.
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.CAMERA},
+                            REQUEST_IMAGE_CAPTURE);
+                } else {
+                    //Tenemos permiso.
+                    capturar();
+                }
+
+
                 break;
             case R.id.btnBuscarContactos:
                 intent=new Intent(Intent.ACTION_VIEW,Uri.parse("content://contacts/" +
@@ -91,4 +131,37 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CALL_PHONE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    // Permiso aceptado y procedemos a realizar la llamada
+                    llamar();
+                else
+                    // Permiso denegado
+                    Toast.makeText(this, "No se ha aceptado el permiso", Toast.LENGTH_SHORT).show();
+                return;
+            // Gestionar el resto de permisos
+            case REQUEST_IMAGE_CAPTURE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    // Permiso aceptado y procedemos a realizar la llamada
+                    capturar();
+                else
+                    // Permiso denegado
+                    Toast.makeText(this, "No se ha aceptado el permiso", Toast.LENGTH_SHORT).show();
+                return;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+    private void capturar() {
+        intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        // Lanzamos la actividad y esperamos su resultado
+        lanzadorActividades.launch(intent);
+    }
+    private void llamar() {
+        intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:(+34)555900800"));
+        startActivity(intent);
+    }
 }
